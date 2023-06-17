3c0b6c87acf26f1bdd3cb5b869fba32ac400bf87
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2023-06-17 20:38:06 +0530

    init

---
+++ src/test/java/com/learn/hibernate6/Hibernate6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.hibernate6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
